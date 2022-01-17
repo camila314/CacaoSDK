00c5b752095db62be7f4704aa5dff90aa669324f
camila314 <camden7306@gmail.com>
2022-01-17 15:25:28 -0600

    base in base

--- SDK/Include/base/Base.hpp
+++ SDK/Include/base/Base.hpp
@@ -7,8 +7,33 @@
 #include <stdint.h>
 #include <gdstdlib.hpp>
 
-uintptr_t getBase();
-inline uintptr_t const base = getBase();
+#if defined(CC_TARGET_OS_MAC)
+	#include <mach-o/dyld.h>
+#elif defined(CC_TARGET_OS_WIN32)
+	#include <windows.h>
+#elif defined(CC_TARGET_OS_IPHONE)
+	#include <mach-o/dyld.h>
+#elif defined(CC_TARGET_OS_ANDROID)
+	#include <dlfcn.h>
+#endif
+
+static uintptr_t base = 0;
+inline uintptr_t getBase() {
+	if (!base) {
+	#if defined(CC_TARGET_OS_MAC)
+		base = _dyld_get_image_vmaddr_slide(0)+0x100000000;
+	#elif defined(CC_TARGET_OS_WIN32)
+		base = reinterpret_cast<uintptr_t>(GetModuleHandle(0));
+	#elif defined(CC_TARGET_OS_IPHONE)
+		base = _dyld_get_image_vmaddr_slide(0)+0x100000000;
+	#elif defined(CC_TARGET_OS_ANDROID)
+		base = reinterpret_cast<uintptr_t>(dlopen("libcocos2dcpp.so", RTLD_LAZY));
+	#else
+		static_assert(false, "Invalid platform");
+	#endif
+	}
+	return base;
+}
 
 #define CACAO_VERSION "3.4.0"
 
